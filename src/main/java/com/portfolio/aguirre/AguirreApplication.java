bf7aeb543fd4c3b1ebaec33d9ea4a600d204036f
Franco Aguirre <francoaguirretrabajo@gmail.com>
2022-10-27 21:45:56 -0300

    Initializer

---
+++ src/main/java/com/portfolio/aguirre/AguirreApplication.java
@@ -0,0 +1,13 @@
+package com.portfolio.aguirre;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AguirreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AguirreApplication.class, args);
+	}
+
+}
