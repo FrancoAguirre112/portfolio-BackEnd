6bddcfb15c3bc0650b5816bc8755112da4cf1751
Franco Aguirre <francoaguirretrabajo@gmail.com>
2022-10-29 23:45:18 -0300

    Login y Logout funciona

--- src/main/java/com/portfolio/aguirre/Controller/PersonaController.java
+++ src/main/java/com/portfolio/aguirre/Controller/PersonaController.java
@@ -4,6 +4,8 @@ import com.portfolio.aguirre.Entity.Persona;
 import com.portfolio.aguirre.Interface.IPersonaService;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -14,6 +16,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:4200")
 public class PersonaController {
     @Autowired IPersonaService ipersonaService;
     
@@ -22,18 +25,21 @@ public class PersonaController {
         return ipersonaService.getPersona();
     }
     
+    @PreAuthorize("hasRole('ADMIN')")
     @PostMapping("/personas/crear")
     public String createPersona(@RequestBody Persona persona){
         ipersonaService.savePersona(persona);
         return "Persona creada";
     }
     
+    @PreAuthorize("hasRole('ADMIN')")
     @DeleteMapping("/personas/borrar/{id}")
     public String deletePersona(@PathVariable Long id){
         ipersonaService.deletePersona(id);
         return "Persona borrada";
     }
     
+    @PreAuthorize("hasRole('ADMIN')")
     @PutMapping("/personas/editar/{id}")
     public Persona editPersona(@PathVariable Long id, 
                                @RequestParam("nombre") String nuevoNombre,
@@ -47,5 +53,10 @@ public class PersonaController {
        
        ipersonaService.savePersona(persona);
        return persona;
+    }
+    
+    @GetMapping("/personas/traer/perfil")
+    public Persona findPersona() {
+        return ipersonaService.findPersona((long)1);
     }
 }
